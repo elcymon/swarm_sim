af5903e456406eabf6df089c28cc94aba39ceab5
elcymon <elcymon@gmail.com>
2018-11-30 11:15:41 +0000

    simulation sequence for RW with extra logged items

--- myQsub_starter.sh
+++ myQsub_starter.sh
@@ -1,15 +1,15 @@
 taskSize=150
 lastPort=0
-qsub hpc_qsub.sh Uniform RW-turnP 0 $lastPort
+qsub hpc_qsub.sh Uniform RW-turnP-xtlog 0 $lastPort
 
 lastPort=$(($lastPort + $taskSize))
-qsub hpc_qsub.sh HalfCluster RW-turnP 0 $lastPort
+qsub hpc_qsub.sh HalfCluster RW-turnP-xtlog 0 $lastPort
 
 lastPort=$(($lastPort + $taskSize))
-qsub hpc_qsub.sh OneCluster RW-turnP 0 $lastPort
+qsub hpc_qsub.sh OneCluster RW-turnP-xtlog 0 $lastPort
 
 lastPort=$(($lastPort + $taskSize))
-qsub hpc_qsub.sh TwoClusters RW-turnP 0 $lastPort
+qsub hpc_qsub.sh TwoClusters RW-turnP-xtlog 0 $lastPort
 
 lastPort=$(($lastPort + $taskSize))
-qsub hpc_qsub.sh FourClusters RW-turnP 0 $lastPort
+qsub hpc_qsub.sh FourClusters RW-turnP-xtlog 0 $lastPort
