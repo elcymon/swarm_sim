1f77513dfa0c18f710b7e3f36973a4d02da5d454
elcymon <elcymon@gmail.com>
2019-01-16 15:02:52 +0000

    updated main.cpp test file

--- sources/comm_models/main.cpp
+++ sources/comm_models/main.cpp
@@ -4,22 +4,28 @@ using namespace std;
 
 int main() {
     //needed for testing the features of CommModels Class
-    CommModels averageFilter = CommModels(40, "average_filter");
+    int q = 40;
+    CommModels averageFilter = CommModels(q, "average_filter");
     
 
-    for (int i = 1; i <= 120; i++) {
+    for (int i = 1; i <= q * 10; i++) {
         double call = (double) i;
-        double repel = call * 2;
-        double tStamp = call / 40;
-        averageFilter.update_comm_signals(i,2*i,i/40);
+        double repel = - call * 2;
+        double tStamp = call / q;
+        averageFilter.update_comm_signals(call,repel,tStamp);
 
         //print values
-        std::cout<<i<<" "
-                    <<"["<<averageFilter.get_value("prev_call_signal")<<","
-                    <<averageFilter.get_value("curr_call_signal")<<"],"
-                    <<"["<<averageFilter.get_value("prev_repel_signal")<<","
-                    <<averageFilter.get_value("curr_repel_signal")<<"]"
-                    <<std::endl;
+        if(i % q == 0){
+            std::cout<<i<<" "
+                        <<"[call: "<<averageFilter.get_value("delta_call_signal")<<" = "
+                        <<averageFilter.get_value("prev_call_signal")<<","
+                        <<averageFilter.get_value("curr_call_signal")<<"],"
+                        <<"[rep: "<<averageFilter.get_value("delta_repel_signal")<<" = "
+                        <<averageFilter.get_value("prev_repel_signal")<<","
+                        <<averageFilter.get_value("curr_repel_signal")<<"]"
+                        
+                        <<std::endl;
+        }
     }
 
     return 0;
