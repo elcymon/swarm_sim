0c05de3f540e0fbd5d642d0d3fe341e6154edda7
elcymon <elcymon@gmail.com>
2019-08-30 21:23:32 +0100

    update hpc_qsub.sh

--- hpc_qsub.sh
+++ hpc_qsub.sh
@@ -43,7 +43,7 @@
 hpc=$1 # true if working on hpc false otherwise
 if ((hpc)); then
     module load singularity
-    folder=/nobackup/scsoo
+    folder=$PWD
 else
     folder=.
     JOB_ID=123
@@ -66,4 +66,4 @@ port_number=$(($SGE_TASK_ID + $5 + 1))
 echo world_name: $world_name, experiment: $experiment, row_shift: $row_shift, paramLine: $paramLine, port_number: $port_number
 mkdir -p $local_loc/$JOB_ID.$SGE_TASK_ID.24core-128G.q $folder/results
 
-singularity exec --bind $folder/results:$PWD/results,$folder/results:$folder/swarm_sim*/results,$local_loc:/local $folder/20190708-libgazebo7-xenial.simg python3 hpc_start_simulation2.py $world_name $experiment $paramLine $SGE_TASK_ID $port_number
+singularity exec --bind $folder:$PWD,$local_loc:/local $folder/20190708-libgazebo7-xenial.simg python3 hpc_start_simulation2.py $world_name $experiment $paramLine $SGE_TASK_ID $port_number
