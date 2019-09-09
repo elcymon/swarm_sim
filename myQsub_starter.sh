c92e3b5f1b179ff68c148b9781aa7d6c6f453840
elcymon <elcymon@gmail.com>
2019-09-09 09:07:54 +0100

    queue size of average filter

--- myQsub_starter.sh
+++ myQsub_starter.sh
@@ -24,23 +24,23 @@ if((! hpc)); then
     
     # ./hpc_qsub.sh $hpc Uniform100m $experiment $paramLine 55$paramLine
 else
-    qsub hpc_qsub.sh $hpc OneCluster $experiment $paramLine 1$paramLine
+    #qsub hpc_qsub.sh $hpc OneCluster $experiment $paramLine 1$paramLine
     
     qsub hpc_qsub.sh $hpc TwoClusters $experiment $paramLine 2$paramLine
     
-    qsub hpc_qsub.sh $hpc FourClusters $experiment $paramLine 3$paramLine
+    #qsub hpc_qsub.sh $hpc FourClusters $experiment $paramLine 3$paramLine
     
-    qsub hpc_qsub.sh $hpc HalfCluster $experiment $paramLine 4$paramLine
+    #qsub hpc_qsub.sh $hpc HalfCluster $experiment $paramLine 4$paramLine
     
-    qsub hpc_qsub.sh $hpc Uniform $experiment $paramLine 5$paramLine
+    #qsub hpc_qsub.sh $hpc Uniform $experiment $paramLine 5$paramLine
     
-    qsub hpc_qsub.sh $hpc OneCluster100m $experiment $paramLine 11$paramLine
+    #qsub hpc_qsub.sh $hpc OneCluster100m $experiment $paramLine 11$paramLine
     
-    qsub hpc_qsub.sh $hpc TwoClusters100m $experiment $paramLine 22$paramLine
+    #qsub hpc_qsub.sh $hpc TwoClusters100m $experiment $paramLine 22$paramLine
     
-    qsub hpc_qsub.sh $hpc FourClusters100m $experiment $paramLine 33$paramLine
+    #qsub hpc_qsub.sh $hpc FourClusters100m $experiment $paramLine 33$paramLine
     
-    qsub hpc_qsub.sh $hpc HalfCluster100m $experiment $paramLine 44$paramLine
+    #qsub hpc_qsub.sh $hpc HalfCluster100m $experiment $paramLine 44$paramLine
     
-    qsub hpc_qsub.sh $hpc Uniform100m $experiment $paramLine 55$paramLine
+    #qsub hpc_qsub.sh $hpc Uniform100m $experiment $paramLine 55$paramLine
 fi
