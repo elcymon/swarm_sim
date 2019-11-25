def8042c1e28ffc77808a9f1b944bacb36963884
elcymon <elcymon@gmail.com>
2019-11-25 10:57:26 +0000

    setup for first complete vision modelling experiment using p_s2s and p_u2s

--- myQsub_starter.sh
+++ myQsub_starter.sh
@@ -4,28 +4,30 @@ paramLine=$2
 experiment=$3
 
 if (( hpc )); then
-    submit=qsub
+    # submit=qsub
+    qsub hpc_qsub.sh $hpc OneCluster $experiment $paramLine 1$paramLine
+
+    qsub hpc_qsub.sh $hpc TwoClusters $experiment $paramLine 2$paramLine
+
+    qsub hpc_qsub.sh $hpc FourClusters $experiment $paramLine 3$paramLine
+
+    qsub hpc_qsub.sh $hpc HalfCluster $experiment $paramLine 4$paramLine
+
+    qsub hpc_qsub.sh $hpc Uniform $experiment $paramLine 5$paramLine
+
+    qsub hpc_qsub.sh $hpc OneCluster100m $experiment $paramLine 11$paramLine
+
+    qsub hpc_qsub.sh $hpc TwoClusters100m $experiment $paramLine 22$paramLine
+
+    qsub hpc_qsub.sh $hpc FourClusters100m $experiment $paramLine 33$paramLine
+
+    qsub hpc_qsub.sh $hpc HalfCluster100m $experiment $paramLine 44$paramLine
+
+    qsub hpc_qsub.sh $hpc Uniform100m $experiment $paramLine 55$paramLine
 else
-    submit=sh
+    # submit=sh
+    sh hpc_qsub.sh $hpc Uniform $experiment $paramLine 5$paramLine
 fi
 
-#qsub hpc_qsub.sh $hpc OneCluster $experiment $paramLine 1$paramLine
 
-# qsub hpc_qsub.sh $hpc TwoClusters $experiment $paramLine 2$paramLine
-
-#qsub hpc_qsub.sh $hpc FourClusters $experiment $paramLine 3$paramLine
-
-#qsub hpc_qsub.sh $hpc HalfCluster $experiment $paramLine 4$paramLine
-
-$submit hpc_qsub.sh $hpc Uniform $experiment $paramLine 5$paramLine
-
-#qsub hpc_qsub.sh $hpc OneCluster100m $experiment $paramLine 11$paramLine
-
-#qsub hpc_qsub.sh $hpc TwoClusters100m $experiment $paramLine 22$paramLine
-
-#qsub hpc_qsub.sh $hpc FourClusters100m $experiment $paramLine 33$paramLine
-
-#qsub hpc_qsub.sh $hpc HalfCluster100m $experiment $paramLine 44$paramLine
-
-#qsub hpc_qsub.sh $hpc Uniform100m $experiment $paramLine 55$paramLine
 
