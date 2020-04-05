65cb89a30ba8644892233038a6938b354ee8059e
elcymon <elcymon@gmail.com>
2020-04-05 10:45:47 +0100

    pipeline experiments to compute swarm pvisible

---
+++ experiments_pipeline.sh
@@ -0,0 +1,9 @@
+./mult_div_submission_loop.sh 1 1 196 Uniform100m var_s2s_u2s_40fps params/params_var_s2s_u2s.csv 0 36
+./mult_div_submission_loop.sh 1 1 196 OneCluster100m var_s2s_u2s_40fps params/params_var_s2s_u2s.csv 200 36
+./mult_div_submission_loop.sh 1 1 196 Uniform var_s2s_u2s_40fps params/params_var_s2s_u2s.csv 400 36
+./mult_div_submission_loop.sh 1 1 196 OneCluster var_s2s_u2s_40fps params/params_var_s2s_u2s.csv 600 36
+
+./mult_div_submission_loop.sh 1 1 196 Uniform100m var_s2s_u2s_1fps params/params_var_s2s_u2s_1fps.csv 800 36
+./mult_div_submission_loop.sh 1 1 196 OneCluster100m var_s2s_u2s_1fps params/params_var_s2s_u2s_1fps.csv 1000 36
+./mult_div_submission_loop.sh 1 1 196 Uniform var_s2s_u2s_1fps params/params_var_s2s_u2s_1fps.csv 1200 36
+./mult_div_submission_loop.sh 1 1 196 OneCluster var_s2s_u2s_1fps params/params_var_s2s_u2s_1fps.csv 1400 36
