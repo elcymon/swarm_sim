d7961cbb6a1e3fc1518f675810e321d04e097dda
elcymon <elcymon@gmail.com>
2020-04-10 22:41:38 +0100

    update qsub to include queue cluster env variable

--- hpc_qsub.sh
+++ hpc_qsub.sh
@@ -70,6 +70,6 @@ swarmsize=$7
 #there should be no repetition of server port or else they will overwrite each other. Adding 1 just to be safe
 port_number=$(( $SGE_TASK_ID + ( $port_shift + $paramLine ) * 32 ))
 echo world_name: $world_name, experiment: $experiment, paramLine: $paramLine, port_number: $port_number
-mkdir -p $local_loc/$JOB_ID.$SGE_TASK_ID.24core-128G.q $folder/results
+mkdir -p $local_loc/$JOB_ID.$SGE_TASK_ID.$QUEUE $folder/results
 
 singularity exec --bind $folder:$PWD,$local_loc:/local $folder/20190708-libgazebo7-xenial.simg python3 hpc_start_simulation2.py $world_name $experiment $param_file $paramLine $SGE_TASK_ID $JOB_ID $port_number $gzmode $swarmsize
