250bfdb48ff6e08c56fa31bdb50b891991146b13
elcymon <elcymon@gmail.com>
2020-05-15 13:50:59 +0100

    setup to run 4pfs 5m 120deg experiments

--- experiments_pipeline.sh
+++ experiments_pipeline.sh
@@ -1,14 +1,14 @@
-./mult_div_submission_loop.sh 1 1 588 Uniform v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 588 OneCluster v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 588 TwoClusters v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 588 FourClusters v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 588 HalfCluster v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 Uniform v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 OneCluster v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 TwoClusters v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 FourClusters v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 HalfCluster v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
 
-./mult_div_submission_loop.sh 1 1 588 Uniform100m v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 588 OneCluster100m v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 588 TwoClusters100m v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 588 FourClusters100m v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 588 HalfCluster100m v5m_s2s_u2s_1fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 Uniform100m v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 OneCluster100m v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 TwoClusters100m v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 FourClusters100m v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 588 HalfCluster100m v5m_s2s_u2s_4fps params/params_var_s2s_u2s_5m_1fps.csv 0 36
 
 
 
