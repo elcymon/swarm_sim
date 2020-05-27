347d1384c6c3786d36a560cbf75b5df6c51dd236
elcymon <elcymon@gmail.com>
2020-05-27 11:23:27 +0100

    scalability experiments setup for n0q1 update port number

--- experiments_pipeline.sh
+++ experiments_pipeline.sh
@@ -12,89 +12,89 @@
 
 
 # ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 0 9
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 0 9
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 0 9
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 0 9
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 0 9
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 1 9
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 2 9
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 3 9
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 4 9
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 0 9
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 0 9
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 0 9
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 0 9
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 0 9
+# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 5 9
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 6 9
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 7 9
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 8 9
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 9 9
 
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 0 16
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 0 16
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 0 16
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 0 16
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 0 16
+# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 10 16
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 11 16
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 12 16
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 13 16
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 14 16
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 0 16
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 0 16
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 0 16
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 0 16
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 0 16
+# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 15 16
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 16 16
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 17 16
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 18 16
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 19 16
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 0 25
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 0 25
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 0 25
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 0 25
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 0 25
+# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 20 25
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 21 25
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 22 25
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 23 25
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 24 25
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 0 25
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 0 25
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 0 25
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 0 25
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 0 25
+# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 25 25
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 26 25
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 27 25
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 28 25
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 29 25
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 0 49
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 0 49
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 0 49
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 0 49
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 0 49
+# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 30 49
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 31 49
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 32 49
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 33 49
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 34 49
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 0 49
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 0 49
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 0 49
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 0 49
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 0 49
+# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 35 49
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 36 49
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 37 49
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 38 49
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 39 49
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 0 64
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 0 64
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 0 64
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 0 64
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 0 64
+# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 40 64
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 41 64
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 42 64
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 43 64
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 44 64
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 0 64
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 0 64
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 0 64
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 0 64
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 0 64
+# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 45 64
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 46 64
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 47 64
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 48 64
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 49 64
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 0 81
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 0 81
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 0 81
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 0 81
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 0 81
+# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 50 81
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 51 81
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 52 81
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 53 81
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 54 81
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 0 81
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 0 81
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 0 81
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 0 81
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 0 81
+# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 55 81
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 56 81
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 57 81
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 58 81
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 59 81
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 0 100
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 0 100
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 0 100
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 0 100
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 0 100
+# ./mult_div_submission_loop.sh 1 814 814 Uniform n0q1_scalability params/param_search.csv 60 100
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster n0q1_scalability params/param_search.csv 61 100
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters n0q1_scalability params/param_search.csv 62 100
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters n0q1_scalability params/param_search.csv 63 100
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster n0q1_scalability params/param_search.csv 64 100
 
-# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 0 100
-# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 0 100
-# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 0 100
-# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 0 100
-# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 0 100
+# ./mult_div_submission_loop.sh 1 814 814 Uniform100m n0q1_scalability params/param_search.csv 65 100
+# ./mult_div_submission_loop.sh 1 814 814 OneCluster100m n0q1_scalability params/param_search.csv 66 100
+# ./mult_div_submission_loop.sh 1 814 814 TwoClusters100m n0q1_scalability params/param_search.csv 67 100
+# ./mult_div_submission_loop.sh 1 814 814 FourClusters100m n0q1_scalability params/param_search.csv 68 100
+# ./mult_div_submission_loop.sh 1 814 814 HalfCluster100m n0q1_scalability params/param_search.csv 69 100
 
 
 
