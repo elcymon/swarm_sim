4d2a09ca5a24e451c1603455ece141f183df06a9
elcymon <elcymon@gmail.com>
2020-11-18 06:55:13 +0000

    setup to do param search for vector RepAtt

--- experiments_pipeline.sh
+++ experiments_pipeline.sh
@@ -1,2 +1,5 @@
-./mult_div_submission_loop.sh 1 1 242 Uniform100m vector params/params.csv 0 36
-./mult_div_submission_loop.sh 1 1 242 OneCluster100m vector params/params.csv 300 36
+./mult_div_submission_loop.sh 1 1 242 Uniform vector params/params.csv 0 36
+./mult_div_submission_loop.sh 1 1 242 OneCluster vector params/params.csv 300 36
+./mult_div_submission_loop.sh 1 1 242 TwoClusters vector params/params.csv 600 36
+./mult_div_submission_loop.sh 1 1 242 FourClusters vector params/params.csv 900 36
+./mult_div_submission_loop.sh 1 1 242 HalfCluster vector params/params.csv 1200 36
