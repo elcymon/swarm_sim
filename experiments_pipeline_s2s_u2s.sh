f1d0e71e7cbb2298d10621365eff8ad1622fb14d
elcymon <elcymon@gmail.com>
2020-12-12 11:26:28 +0000

    vector u2s search

--- experiments_pipeline_s2s_u2s.sh
+++ experiments_pipeline_s2s_u2s.sh
@@ -1,11 +1,11 @@
 fps=1fps
-./mult_div_submission_loop.sh 1 1 196 Uniform vector params/params_var_s2s_u2s_5m120deg_$fps.csv 0 36
-./mult_div_submission_loop.sh 1 1 196 OneCluster vector params/params_var_s2s_u2s_5m120deg_$fps.csv 196 36
-./mult_div_submission_loop.sh 1 1 196 TwoClusters vector params/params_var_s2s_u2s_5m120deg_$fps.csv 392 36
-./mult_div_submission_loop.sh 1 1 196 FourClusters vector params/params_var_s2s_u2s_5m120deg_$fps.csv 488 36
-./mult_div_submission_loop.sh 1 1 196 HalfCluster vector params/params_var_s2s_u2s_5m120deg_$fps.csv 684 36
-./mult_div_submission_loop.sh 1 1 196 Uniform100m vector params/params_var_s2s_u2s_5m120deg_$fps.csv 880 36
-./mult_div_submission_loop.sh 1 1 196 OneCluster100m vector params/params_var_s2s_u2s_5m120deg_$fps.csv 1076 36
-./mult_div_submission_loop.sh 1 1 196 TwoClusters100m vector params/params_var_s2s_u2s_5m120deg_$fps.csv 1272 36
-./mult_div_submission_loop.sh 1 1 196 FourClusters100m vector params/params_var_s2s_u2s_5m120deg_$fps.csv 1468 36
-./mult_div_submission_loop.sh 1 1 196 HalfCluster100m vector params/params_var_s2s_u2s_5m120deg_$fps.csv 1664 36
+./mult_div_submission_loop.sh 1 1 196 Uniform vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 0 36
+./mult_div_submission_loop.sh 1 1 196 OneCluster vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 196 36
+./mult_div_submission_loop.sh 1 1 196 TwoClusters vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 392 36
+./mult_div_submission_loop.sh 1 1 196 FourClusters vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 488 36
+./mult_div_submission_loop.sh 1 1 196 HalfCluster vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 684 36
+./mult_div_submission_loop.sh 1 1 196 Uniform100m vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 880 36
+./mult_div_submission_loop.sh 1 1 196 OneCluster100m vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 1076 36
+./mult_div_submission_loop.sh 1 1 196 TwoClusters100m vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 1272 36
+./mult_div_submission_loop.sh 1 1 196 FourClusters100m vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 1468 36
+./mult_div_submission_loop.sh 1 1 196 HalfCluster100m vector_5m120 params/params_var_s2s_u2s_5m120deg_$fps.csv 1664 36
